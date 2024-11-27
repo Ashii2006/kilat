502c599d9536759a181883d49e0a0be5b1ff94cb
Ashii2006 <christineash16@gmail.com>
2024-11-27 14:15:09 +0800

    Add files via upload

---
+++ atm.js
@@ -0,0 +1,11 @@
+let currentbalance = 5000;
+
+function withdraw() {
+    window.alert("Please input a valid amount.");
+}
+function deposit() {
+    window.alert("Please input a valid amount.");
+}
+function exit() {
+    window.alert("Thank you for using this ATM. See you again!!");
+}
