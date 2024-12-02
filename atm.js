653aca0d11869a32e6a1358e5bf02659d26436f6
Ashii2006 <christineash16@gmail.com>
2024-12-02 14:41:11 +0800

    Add files via upload

--- atm.js
+++ atm.js
@@ -1,10 +1,32 @@
 let currentbalance = 5000;
 
 function withdraw() {
-    window.alert("Please input a valid amount.");
+   let withdrawalamount = parseFloat(prompt("Enter amount to withdraw:"));
+   if( isNaN (withdrawalamount) || withdrawalamount <=0) {
+    window.alert("Invalid amount. Please enter a valid amount.");
+   }
+   else if (withdrawalamount > currentbalance) {
+    window.alert(`Insufficient balance. Your current balance is ₱${currentbalance}.`);
+   }
+   else {
+    currentbalance -= withdrawalamount;
+    window.alert(`Withdrawal successful. Your new balance is ₱${currentbalance}.`);
+   }
+
 }
 function deposit() {
-    window.alert("Please input a valid amount.");
+    let depositamount = parseFloat(prompt("Enter amount to deposit."));
+    if(isNaN (depositamount) || depositamount <= 0){
+        window.alert("Invalid amount. Please enter a valid amount.");
+    }
+    else {
+        currentbalance += depositamount;
+        window.alert(`Deposit successful. Your new balance is ₱${currentbalance}.`);
+    }
+ 
+}
+function checkbalance(){
+    window.alert(`Your current balance is ₱${currentbalance}`);
 }
 function exit() {
     window.alert("Thank you for using this ATM. See you again!!");
