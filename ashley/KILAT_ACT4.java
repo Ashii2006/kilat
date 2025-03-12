66ad344b6eded2a36aef3df553b39f3e23671b01
Ashii2006 <christineash16@gmail.com>
2025-03-12 19:32:59 +0800

    Update KILAT_ACT4.java

--- ashley/KILAT_ACT4.java
+++ ashley/KILAT_ACT4.java
@@ -1,15 +1,26 @@
 import javax.swing.JOptionPane;
 
-public class ATMWithdrawalSystem {
+public class KILAT_ACT4 {
     public static void main(String[] args) {
+ 
         double balance = 1000;
+        checkBalance(balance);
+        
         String input = JOptionPane.showInputDialog("Enter amount to withdraw: ");
         double withdrawAmount = Double.parseDouble(input);
-        balance = withdraw(balance, withdrawAmount);
+        double newBalance = withdraw(balance, withdrawAmount);
+
+        if (newBalance != balance) { 
+            balance = newBalance;
+        } else {
+            withdrawAmount = 0;
+        }
         
         JOptionPane.showMessageDialog(null, "Account Info:\n" +
                                       "Last Withdrawn Amount: $" + withdrawAmount + "\n" +
                                       "Remaining Balance: $" + balance);
+            
+        
     }
 
     public static void checkBalance(double balance) {
@@ -19,10 +30,11 @@ public class ATMWithdrawalSystem {
     public static double withdraw(double balance, double withdrawAmount) {
         if (withdrawAmount > balance) {
             JOptionPane.showMessageDialog(null, "Insufficient balance. Your balance is $" + balance);
+            return balance;
         } else {
             balance -= withdrawAmount;
             JOptionPane.showMessageDialog(null, "Withdrawal successful! You withdrew $" + withdrawAmount);
+            return balance;
         }
-        return balance;
     }
 }
